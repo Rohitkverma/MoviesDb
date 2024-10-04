7b8bf9eaae533bf769c3ad57e9594e416c6b7021
Deepak kumar <deepak7589467031@gmail.com>
2024-10-04 18:24:46 +0530

    first commit

---
+++ src/Components/CardMovies/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { img_300, img_not_available } from "../../Config";
+const CardMoviesComponents = ({ data }) => {
+
+  const title = data.original_title || data.name;
+  const id = data.id;
+  const ImageURL = data.poster_path
+    ? img_300 + data.poster_path
+    : img_not_available;
+  const release_date = data.release_date || data.first_air_date;
+  const vote_average = parseInt(data.vote_average);
+  return (
+    <>
+      <div className="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-6 mb-4">
+        <Link to={`/details/${id}`} className="video-thumb font-medium shadow-lg p-2">
+          <figure className="video-image">
+            <span>
+              <img src={ImageURL} alt={title} />
+            </span>
+          </figure>
+          <div className="video-content">
+            <h3 className="name text-center">{title}</h3>
+            <h3 className="name text-center" style={{ fontSize: "12px" }}>
+              Rating: {vote_average}
+            </h3>
+          </div>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default CardMoviesComponents;
